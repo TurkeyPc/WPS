fa9a9723f83a42e623601c9363c970ad2dab1bf3
takashi iyama <takkun.iyama@nifty.com>
2019-01-14 08:30:46 +0900

    復旧ツール追加

---
+++ Gomenne.ps1
@@ -0,0 +1,17 @@
+﻿#なんしかセキュリティで実行できないのはつまんないのでこれでよしなに。
+Set-ExecutionPolicy RemoteSigned
+
+#別窓開きレジストリ登録関数
+function CreateOpenNewItem($xlsx){
+    Set-ItemProperty $xlsx -name "(default)" -value "open"
+}
+
+#HKEY_CLASSES_ROOTをPSDriveのHKCRに割り当てる
+#通常はNew-PSDriveだけでいいが
+#ISEで2回目以降にエラー出るのがうっとうしいのでいったんHKCRの割り当てを解除してからNew-PSDriveしている
+$rt = "HKCR"
+Remove-PSDrive -Name $rt
+$v = New-PSDrive -PSProvider Registry -Name $rt -Root HKEY_CLASSES_ROOT
+
+CreateOpenNewItem ($rt + ":\Excel.Sheet.12\shell")
+CreateOpenNewItem ($rt + ":\Excel.Sheet.8\shell")
